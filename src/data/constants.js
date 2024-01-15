bf88cb272eb69dd46898ce86b812d3e7b4570aca
Cristian Gomez <134754887+Cristian-Maxi@users.noreply.github.com>
2024-01-15 17:34:07 -0300

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "Soy una persona motivada y versátil, siempre dispuesta a enfrentar nuevos desafíos. Estoy dispuesto a ofrecer resultados de alta calidad, valiendome de una actitud positiva y una mentalidad de crecimiento, estoy listo para hacer una contribución significativa y lograr grandes cosas.",
   github: "https://github.com/Cristian-Maxi",
   resume:
-    "https://drive.google.com/file/d/1JBvVRu_kDRQOqcq1018kdxmcOuMncTRr/view?usp=drive_link",
+    "https://drive.google.com/file/d/1pXUx0uxPYGRRptcP9QA4nh2rN8ObecPX/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/cristian-gomez-montenegro/",
   twitter: "https://twitter.com/CristianMaxi06",
   insta: "https://www.instagram.com/cristian_maxi_gomez/",
